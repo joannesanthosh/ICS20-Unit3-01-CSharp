ab3cdda09dfedf7642f32b3336084f98a81b4b85
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-11 18:13:00 +0000

    add units

--- main.cs
+++ main.cs
@@ -1,7 +1,7 @@
 // Created by: Joanne Santhosh
-// Created on: Mar 2022
+// Created on: May 2022
 //
-// This program calculates the area of a trapezoid
+// This program tells user if they can enter the museum
 
 using System;
 
@@ -9,10 +9,11 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // This program calculates the area of a trapezoid
+        // This program tells user if they can enter the museum
         int aBaseOfTrapezoid;
         int bBaseOfTrapezoid;
         int heightOfTrapezoid;
+      
         Console.WriteLine("This program calculates the area of a trapezoid");
         Console.WriteLine("");
 
@@ -24,7 +25,7 @@ class Program
         heightOfTrapezoid = (Convert.ToInt32(Console.ReadLine()));
 
         Console.WriteLine("");
-        Console.WriteLine("The Area of the trapezoid is: " + heightOfTrapezoid * (aBaseOfTrapezoid + bBaseOfTrapezoid / 2));
+        Console.WriteLine("The Area of the trapezoid is: " + heightOfTrapezoid * (aBaseOfTrapezoid + bBaseOfTrapezoid / 2) + "cm²");
 
         Console.WriteLine("\nDone.");
     }
